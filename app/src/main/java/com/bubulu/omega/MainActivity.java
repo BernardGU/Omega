ccada5370bc89564764151a96274b0d22d11b16f
Emca27 <emi.aguirre@hotmail.com>
2019-08-24 17:37:00 -0500

    Boton se sale del app

--- app/src/main/java/com/bubulu/omega/MainActivity.java
+++ app/src/main/java/com/bubulu/omega/MainActivity.java
@@ -2,12 +2,27 @@ package com.bubulu.omega;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button LB_Reportar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LB_Reportar = (Button) findViewById(R.id.LB_Reportar);
+
+        LB_Reportar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                finish();
+            }
+
+        });
     }
 }
