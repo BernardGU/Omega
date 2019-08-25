6baf5946fb2deac506b692775d82c285801ce408
Carlos <carlosemiliobravomarquez@gmail.com>
2019-08-25 06:55:06 -0500

    latest

--- app/src/main/java/com/bubulu/omega/ThirdActivity_Feed.java
+++ app/src/main/java/com/bubulu/omega/ThirdActivity_Feed.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.ArrayList;
 
 public class ThirdActivity_Feed extends AppCompatActivity {
+
+    FirebaseFirestore db;
 
     private ListView lvItems;
     private Adaptador adaptador;
@@ -24,6 +28,11 @@ public class ThirdActivity_Feed extends AppCompatActivity {
 
     private ArrayList<Entidad> GetArrayItems() {
         ArrayList<Entidad> listItems = new ArrayList<>();
+
+        db = FirebaseFirestore.getInstance();
+
+
+
         listItems.add(new Entidad(R.drawable.omegalogo, "TITULO 1", "CONTENIDO 1"));
         listItems.add(new Entidad(R.drawable.omegalogo, "TITULO 2", "CONTENIDO 2"));
         listItems.add(new Entidad(R.drawable.omegalogo, "TITULO 3", "CONTENIDO 3"));
