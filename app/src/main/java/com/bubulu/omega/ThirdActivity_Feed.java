fd6a0a079d7e7259f835ef4a7da07fe98cdb9236
Emca27 <emi.aguirre@hotmail.com>
2019-08-24 19:06:05 -0500

    Nueva Activity Feed

---
+++ app/src/main/java/com/bubulu/omega/ThirdActivity_Feed.java
@@ -0,0 +1,14 @@
+package com.bubulu.omega;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThirdActivity_Feed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third__feed);
+    }
+}
