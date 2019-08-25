5cd229326f24471bf44d066725919fb432361023
Emca27 <emi.aguirre@hotmail.com>
2019-08-25 05:45:37 -0500

    Lista

---
+++ app/src/main/java/com/bubulu/omega/Adaptador.java
@@ -0,0 +1,53 @@
+package com.bubulu.omega;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Adaptador extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<Entidad> listItems;
+
+    public Adaptador(Context context, ArrayList<Entidad> listItems) {
+        this.context = context;
+        this.listItems = listItems;
+    }
+
+    @Override
+    public int getCount() {
+        return listItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return listItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Entidad Item = (Entidad) getItem(position);
+
+        convertView = LayoutInflater.from(context).inflate(R.layout.item, null);
+        ImageView imgFoto = (ImageView) convertView.findViewById(R.id.imgFoto);
+        TextView tvTitulo = (TextView) convertView.findViewById(R.id.tvTitulo);
+        TextView tvContenido = (TextView) convertView.findViewById(R.id.tvContenido);
+
+        imgFoto.setImageResource(Item.getImgFoto());
+        tvTitulo.setText(Item.getTitulo());
+        tvContenido.setText(Item.getContenido());
+
+        return convertView;
+    }
+}
