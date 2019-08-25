9e464f1d2ffafbf09bb6c6ee93ee316977ca1ed2
BernardGU <bernardoc.gutierrezc@gmail.com>
2019-08-25 04:18:25 -0500

    Dog class finished and updated

--- app/src/main/java/com/bubulu/omega/Dog.java
+++ app/src/main/java/com/bubulu/omega/Dog.java
@@ -23,27 +23,35 @@ public class Dog {
         this.breed = "";
         this.description = "";
         this.imageLink = "";
+        this.perdido = false;
+        this.encontrado = false;
     }
 
     public Dog(Map<String, Object> readDog) {
-        this.id = (int) readDog.get("id");
+        this.id = (long) readDog.get("id");
         this.name = (String) readDog.get("name");
         this.breed = (String) readDog.get("breed");
         this.description = (String) readDog.get("description");
         this.imageLink = (String) readDog.get("imageLink");
+        this.perdido = (Boolean) readDog.get("perdido");
+        this.encontrado = (Boolean) readDog.get("encontrado");
     }
 
     public String getBreed() {return breed;}
     public String getDescription() {return description;}
     public String getName() {return name;}
     public String getImageLink() {return imageLink;}
+    public Boolean getEncontrado() {return encontrado;}
+    public Boolean getPerdido() {return perdido;}
 
     public void setName(String name) {this.name = name;}
     public void setBreed(String breed) {this.breed = breed;}
     public void setDescription(String description) {this.description = description;}
     public void setImageLink(String imageLink) {this.imageLink = imageLink;}
+    public void setEncontrado(Boolean encontrado) {this.encontrado = encontrado;}
+    public void setPerdido(Boolean perdido) {this.perdido = perdido;}
 
-    public Dog readFromDatabase(FirebaseFirestore db, int id) {
+    public Dog readFromDatabase(FirebaseFirestore db, long id) {
         DocumentReference docRef = db.collection("Dog").document(String.valueOf(id));
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -64,7 +72,7 @@ public class Dog {
         return dogAux;
     }
 
-    private int generateId(FirebaseFirestore db) {
+    public static void generateId(FirebaseFirestore db) {
         DocumentReference docRef = db.collection("Dog").document("Values");
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -72,7 +80,8 @@ public class Dog {
                 if (task.isSuccessful()) {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
-                        iAux = (int) document.get("nextID");
+                        Map<String, Object> d = document.getData();
+                        lAux = (long) d.get("nextID");
                     } else {
                         Log.d("Dog", "Couldn't find new ID");
                     }
@@ -81,12 +90,25 @@ public class Dog {
                 }
             }
         });
-
-        return iAux;
     }
-    public void writeToDatabase(FirebaseFirestore db) {
+
+    private static void updateID(FirebaseFirestore db) {
         Map<String, Object> d = new HashMap<>();
-        d.put("id", (this.id != -1)? this.id : generateId(db));
+        d.put("nextID", lAux + 1);
+
+        db.collection("Dog")
+                .document("Values")
+                .set(d);
+    }
+
+    public void writeToDatabase(FirebaseFirestore db) {
+        if(this.id == -1) {
+            this.id = lAux;
+            updateID(db);
+        }
+
+        Map<String, Object> d = new HashMap<>();
+        d.put("id", String.valueOf(this.id));
         d.put("name", this.name);
         d.put("breed", this.breed);
         d.put("description", this.description);
@@ -97,12 +119,14 @@ public class Dog {
                 .set(d);
     }
 
-    private int id;
+    private long id;
     private String name;
     private String breed;
     private String description;
     private String imageLink;
+    private Boolean perdido;
+    private Boolean encontrado;
 
-    public Dog dogAux;
-    public int iAux;
+    public static Dog dogAux;
+    public static long lAux;
 }
